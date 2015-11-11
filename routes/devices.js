4210a1ffcc76009ccecc2373e7333a1ab31690a3
Jason Chapin <jason@lab1337.com>
2015-11-11 08:06:27 +0100

    WIP implementing post call to device

--- routes/devices.js
+++ routes/devices.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
+var http = require('http');
+var xmlparser = require('express-xml-bodyparser');
 
 fs.readFile(path.join(__dirname, '../models/devices.json'), function(err, data) {
   data = JSON.parse(data);
@@ -45,14 +47,28 @@ fs.readFile(path.join(__dirname, '../models/devices.json'), function(err, data) 
   router.post('/:did/:dvalue', function(req, res, next) {
     var device = data.filter(function(dev) {
       return dev.id == req.params.did;
-    }).map(function(dev) {
-      return {
-        description: dev.description,
-        type: dev.type,
-        status: dev.status
-      };
     }).pop();
     // TODO call device api to update
+
+    var options = {
+      host: device.address,
+      //path: "/digitaloutput/" + device.channel + "/" + req.params.dvalue + "/value",
+      path: "/digitaloutput/all/value",
+      port: device.port
+    };
+    console.log(options);
+    http.get(options, function(http_res) {
+      var http_res_data = '';
+      http_res.on("data", function(chunk) {
+        http_res_data += chunk;
+      });
+      http_res.on("end", function() {
+        //http_res_data =
+
+          console.log(http_res_data);
+      });
+    });
+
     if (device) {
       res.status(200);
     } else {

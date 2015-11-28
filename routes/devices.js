dfc362250ef980f6a6f7ba3a062f38e404b47a12
Jason Chapin <jason@lab1337.com>
2015-11-28 16:59:04 -0500

    Accessing digitalinputs via GET

--- routes/devices.js
+++ routes/devices.js
@@ -8,6 +8,7 @@ fs.readFile(path.join(__dirname, '../models/devices.json'), function(err, data) 
   data = JSON.parse(data);
 
   router.get('/', function(req, res, next) {
+    console.log(data);
     var devs = data.map(function(dev) {
       return {
         id: dev.id,
@@ -22,23 +23,41 @@ fs.readFile(path.join(__dirname, '../models/devices.json'), function(err, data) 
 
 
   router.get('/:did', function(req, res, next) {
+    var device_id = req.params.did;
     var device = data.filter(function(dev) {
-      return dev.id == req.params.did;
-    }).map(function(dev) {
-      return {
-        description: dev.description,
-        type: dev.type,
-        status: dev.status
-      };
+      return dev.id == device_id;
     }).pop();
-
+console.log(device);
     if (device) {
-      res.status(200);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(device);
+      var api_mothod;
+      if (device.type == "light") {
+        api_method = 'digitaloutput';
+      } else if (device.type == "lock") {
+        api_method = 'digitalinput';
+      }
+      console.log("http://root:00000000@" + device.address + ":" + device.port + "/" + api_method + "/" + device.channel + "/value");
+      rest.get("http://root:00000000@" + device.address + ":" + device.port + "/" + api_method + "/" + device.channel + "/value")
+        .on("error", function(err) {
+          res.status(500);
+        })
+        .on("success", function(body, dev_res) {
+      	  console.log("BODY " + body);
+      	  if (body.indexOf('status="OK"') > 0) {
+            // update internal data structure
+            //[device_id]['status'] = parseInt(req.params.dvalue);
+            res.status(200);
+            res.send({
+              id: device.id,
+              description: device.description,
+              status: device.status
+            });
+          } else {
+            res.status(404);
+          }
+        });
+
     } else {
       res.status(404);
-      res.end();
     }
   });
 
@@ -68,6 +87,8 @@ fs.readFile(path.join(__dirname, '../models/devices.json'), function(err, data) 
             // update internal data structure
             data[req.params.did]['status'] = parseInt(req.params.dvalue);
             res.status(204);
+
+
           } else {
             res.write(JSON.stringify(body));
             res.status(404);
